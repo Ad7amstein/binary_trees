49df0abd832bcbd91e0052dd4b18fd6d04cfa05b
abdelfattah <abdelfatahwenny@yahoo.com>
2023-11-02 18:49:39 -0700

    added a function to get a node sibling

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_t - function to retreive the binary tree sibling
+ * @node: the node to get its sibling
+ * Return: the sibling node or NULL otherwise
+*/
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node == node->parent->left)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+}
